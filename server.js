07fe939b8d0d922a950696703e58a64ff995e1c8
HipsterPenguin <wyss.craiga@gmail.com>
2023-02-16 11:44:40 -0600

    Fixed styling to match convetions

--- server.js
+++ server.js
@@ -1,41 +1,39 @@
 const newFolderName = function (folders) {
+  if (folders.length === 0) {
+    return 'New Folder'
+  }
 
-    if (folders.length === 0) {
-        return 'New Folder'
+  const regex = /New Folder \((\d+)\)/
+  const numbers = []
+  let containsNewFolder = false
+
+  for (let i = 0; i < folders.length; i++) {
+    if (folders[i] === 'New Folder') {
+      numbers.push(1)
+      containsNewFolder = true
+    } else if (regex.test(folders[i])) {
+      numbers.push(parseInt(folders[i].match(regex)[1]))
     }
-    
-    let regex = /New Folder \((\d+)\)/
-    let numbers = [];
-    let containsNewFolder = false;
-    
-    for (let i = 0; i < folders.length; i++) {
-        if (folders[i] === 'New Folder') {
-            numbers.push(1)
-            containsNewFolder = true
-        }
-        else if (regex.test(folders[i])) {
-            numbers.push(parseInt(folders[i].match(regex)[1]))
-        }
+  }
+
+  if (numbers.length === 0) {
+    return 'New Folder'
+  }
+
+  numbers.sort()
+  let nextNumber = numbers[0]
+
+  if (nextNumber !== 1 || !containsNewFolder) {
+    return 'New Folder'
+  }
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] === nextNumber + 1) {
+      nextNumber++
     }
+  }
 
-    if (numbers.length === 0) {
-        return 'New Folder'
-    }
-
-    numbers.sort()
-    nextNumber = numbers[0]
-
-    if (nextNumber !== 1 || !containsNewFolder) {
-        return 'New Folder'
-    }
-
-    for (let i = 1; i < numbers.length; i++) {
-        if (numbers[i] === nextNumber + 1) {
-            nextNumber++
-        }
-    }
-
-    return `New Folder (${nextNumber + 1})`
+  return `New Folder (${nextNumber + 1})`
 }
 
 module.exports = { newFolderName }
